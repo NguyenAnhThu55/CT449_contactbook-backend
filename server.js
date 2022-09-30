a1d53e8fa52a5f0eeb34652bc8747b827eae604a
User Name <nguyenanhthu55138@gmail.com>
2022-09-30 11:43:00 +0700

    Cai dat cac handler truy xuat CSDL

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const app = require('./app');
 const config = require('./app/config');
+const MongoDB = require('./app/utils/mongodb.util');
 
-// app.listen(3000, '0.0.0.0', function() {
-//     console.log('server listening on:  ' + 3000);
-// });
-const PORT = config.app.port ;
-app.listen(PORT,()=>{
-    console.log(`server listening on ${PORT}`);
-});
+async function startServer(){
+    try{
+        await MongoDB.connect(config.db.uri);
+        console.log('Connected to the database!');
+        
+        const PORT = config.app.port ;
+        app.listen(PORT,()=>{
+            console.log(`server is running on port ${PORT}`);
+         });
+
+    }
+    catch(error){
+        console.log('Cannot connecting to the database!', error);
+        process.exit();
+    }
+}
+startServer();
